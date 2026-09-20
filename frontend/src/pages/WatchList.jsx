e8c38b2c353d6ba52d1eeabfce9abbbaa4e42b3e
Shumail-Gul <shumailgul140@gmail.com>
2026-09-20 11:56:37 +0500

    Add margin to WatchList movie grid

--- frontend/src/pages/WatchList.jsx
+++ frontend/src/pages/WatchList.jsx
@@ -71,7 +71,7 @@ function WatchList() {
 			)}
 
 			{!loading && !error && movies.length > 0 && (
-				<div className="row row-cols-1 row-cols-md-2 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 g-4">
+				<div className="row row-cols-1 row-cols-md-2 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 g-4 mx-3">
 					{movies.map((movie) => (
 						<Cards
 							key={movie.id}
