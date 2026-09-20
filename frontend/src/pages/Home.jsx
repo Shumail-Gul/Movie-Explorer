f1c4f012676e33f461673c485d69dc6cacea8628
Shumail-Gul <shumailgul140@gmail.com>
2026-09-20 10:05:43 +0500

    Reduce movie card columns from 5 to 4

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -113,7 +113,7 @@ function Home() {
 						<p className="text-muted">Try searching for another movie.</p>
 					</div>
 				) : (
-						<div className="row row-cols-1 row-cols-md-2 row-cols-sm-2 row-cols-lg-3 row-cols-xl-5 g-3 my-2">
+						<div className="row row-cols-1 row-cols-md-2 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 g-3 my-2">
 							{movies.map((movie, index) => (
 								<Cards
 									key={movie.id}
